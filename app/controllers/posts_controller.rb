666d20f9c01920e37d53b9ce7bbf3c7083f7756d
Daniel Mateos <daniel@mateos.cc>
2014-03-06 00:51:06 +1030

    edit pages and togo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,10 @@ class PostsController < ApplicationController
     redirect_to @post
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
