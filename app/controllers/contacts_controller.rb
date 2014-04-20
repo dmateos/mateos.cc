a731e3dd6d9b33bed72fec227fc62245013fcaa0
Daniel Mateos <daniel@mateos.cc>
2014-04-20 17:27:30 +0930

    working now

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,12 +3,19 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
 
-  def new
-
-  end
-
   def create
+    @contact = Contact.new
+    @contact.name = params[:contact][:name]
+    @contact.email = params[:contact][:email]
+    @contact.subject = params[:contact][:subject]
+    @contact.message = params[:contact][:message]
 
+    if @contact.save
+      flash[:notice] = 'Your message has been sent'
+    else
+      flash[:alert] = 'Validation error'
+    end
+    redirect_to '/contact'
   end
 
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,14 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+     record.errors[attribute] << (options[:message] || "is not an email")
+   end
+  end
+end
+
 class Contact < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true, email: true
+  validates :subject, presence: true
+  validates :message, presence: true
 end
